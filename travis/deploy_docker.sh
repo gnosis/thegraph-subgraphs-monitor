559391e4cfcaf1cbc81ad8661a158d9388d044c2
Luarx <22997139+luarx@users.noreply.github.com>
2020-04-03 02:49:11 +0200

    Enable Travis CI

---
+++ travis/deploy_docker.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -euo pipefail
+
+if [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
+    echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+
+    if [ "$1" = "master" ]; then
+        # STAGING IMAGES
+        # If image does not exist, don't use cache
+        docker pull gnosispm/$DOCKERHUB_PROJECT:$1 && \
+        docker build -t $DOCKERHUB_PROJECT -f Dockerfile . --cache-from gnosispm/$DOCKERHUB_PROJECT:$1 || \
+        docker build -t $DOCKERHUB_PROJECT -f Dockerfile .
+    else
+        # TAGGED images
+        # If image does not exist, don't use cache
+        docker pull gnosispm/$DOCKERHUB_PROJECT:staging && \
+        docker build -t $DOCKERHUB_PROJECT -f Dockerfile . --cache-from gnosispm/$DOCKERHUB_PROJECT:staging || \
+        docker build -t $DOCKERHUB_PROJECT -f Dockerfile .
+    fi
+
+    docker tag $DOCKERHUB_PROJECT gnosispm/$DOCKERHUB_PROJECT:$1
+    docker push gnosispm/$DOCKERHUB_PROJECT:$1
+fi
